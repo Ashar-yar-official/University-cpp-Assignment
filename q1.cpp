d1fd4cd84b222d5545d4f7460d4d0c592df53f3d
Ashar yar <asharyar1096@gmail.com>
2026-04-14 16:19:25 +0500

    Add basic input/output program in q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Enter value for y = ";
+	cin>>y;
+	if (y==10)
+	x=0;
+	else
+	x=1;
+	
+	cout<<"value of x = " <<x;
+	return 0;
+}
